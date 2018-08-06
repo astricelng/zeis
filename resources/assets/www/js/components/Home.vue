41bcd2c21cbeeae52da0b448dade8ef2921cac3e
astricelng <astricelng@gmail.com>
2018-08-06 14:39:32 -0500

    doble home

--- resources/assets/www/js/components/Home.vue
+++ resources/assets/www/js/components/Home.vue
@@ -1,68 +1,4 @@
 <template>
-	
-	<div id="intro" class="intro__back" :style="{ backgroundImage: 'url(' + url + 'assets/www/images/intro_bg.jpg' + ')' }">
-	  <div class="intro-body">
-	    <div class="container container_home">
-	      <div class="texto_intro">
-	        <img :src="url + 'assets/www/images/letras_intro_'+lng+'.png'">  
-	      </div>
-	      <div class="logo_intro">
-	        <img :src="url + 'assets/www/images/logo_intro.png'">  
-	      </div>
-	    </div>
-	  </div>
-	</div>
-
-</template>
-
-<script>
-
-    export default {
-
-         data() {
-            return { 
-                url: window.i18n.endpoints.url,
-                lng: window.i18n.endpoints.language,
-                contentT1: new TimelineMax(),
-            }
-        },
-
-        methods: {
-
-        },
-
-        mounted () { 
-
-            let $intro = $('#intro');
-            const controller = new ScrollMagic.Controller();
-
-            this.introTop       = $intro.find('.texto_intro');
-            this.introLogo    	= $intro.find('.logo_intro');
-
-            this.contentT1
-            .fromTo(this.introLogo, 1,
-                {y: 100, autoAlpha: "0"},
-                {y: 0, autoAlpha: "1", ease: Circ.easeInOut}, "+=1"
-            )
-            .fromTo(this.introTop, 0.8,
-                {autoAlpha: "0"},
-                {autoAlpha: "1", ease: Circ.easeInOut }
-            );
-
-            var scene = new ScrollMagic.Scene({
-                triggerElement: '#intro', 
-                triggerHook: 0.1,
-            })
-            .setTween(this.contentT1)
-            .addTo(controller);
-
-    
-        } //Close Mounted
-
-    }
-</script>
-
-<!--template>
 
     <div class="padre">
         <div class="area_derecha" v-on:click="setHome(1)"></div>
@@ -126,4 +62,4 @@
         } //Close Mounted
 
     }
-</script-->
+</script>
